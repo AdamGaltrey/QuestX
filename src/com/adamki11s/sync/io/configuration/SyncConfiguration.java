c1e685e55a32b66be01f9033bb0c76b8569227f1
Adamki11s <adam@adamki11s.co.uk>
2013-01-29 18:27:45 +0000

    Improved default value returns

--- src/com/adamki11s/sync/io/configuration/SyncConfiguration.java
+++ src/com/adamki11s/sync/io/configuration/SyncConfiguration.java
@@ -3,7 +3,6 @@ package com.adamki11s.sync.io.configuration;
 import java.io.File;
 import java.io.IOException;
 import java.util.LinkedHashMap;
-import java.util.Map.Entry;
 import java.util.UUID;
 
 import com.adamki11s.io.IDENTIFIER;
