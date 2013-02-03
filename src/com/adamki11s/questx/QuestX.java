caebb207ee1fd42bdddbfbfff8a72e414d43bfea
Adamki11s <adam@adamki11s.co.uk>
2013-02-03 22:07:15 +0000

    Added plugin Metrics

--- src/com/adamki11s/questx/QuestX.java
+++ src/com/adamki11s/questx/QuestX.java
@@ -1,6 +1,7 @@
 package com.adamki11s.questx;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 import net.milkbowl.vault.economy.Economy;
@@ -8,7 +9,6 @@ import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.RegisteredServiceProvider;
@@ -26,6 +26,7 @@ import com.adamki11s.events.TagColourEvent;
 import com.adamki11s.io.GeneralConfigData;
 import com.adamki11s.io.InitialSetup;
 import com.adamki11s.io.WorldConfigData;
+import com.adamki11s.metrics.Metrics;
 import com.adamki11s.npcs.NPCHandler;
 import com.adamki11s.npcs.loading.FixedLoadingTable;
 import com.adamki11s.payload.ExtractPayload;
@@ -56,6 +57,8 @@ public class QuestX extends JavaPlugin {
 	public static Plugin p;
 	public static String version;
 	public static File f;
+	
+	Metrics metrics;
 
 	public static synchronized void logMSG(String msg) {
 		log.info("[QuestX] " + msg);
@@ -129,6 +132,18 @@ public class QuestX extends JavaPlugin {
 		}
 
 		InitialSetup.run();
+		
+		try {
+		    metrics = new Metrics(this);
+		    boolean result = metrics.start();
+		    if(result){
+		    	QuestX.logMSG("Plugin Metrics connection was successful!");
+		    } else {
+		    	QuestX.logMSG("Plugin Metrics connection denied, config may be set to opt-out:false");
+		    }
+		} catch (IOException e) {
+		    QuestX.logError("Failed to submit statistics!");
+		}
 
 		if (GeneralConfigData.isCheckingUpdates()) {
 			Updater u;
