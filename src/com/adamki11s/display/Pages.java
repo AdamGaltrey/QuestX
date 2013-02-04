9409509292b9124b759590b1242ef118d47b2abe
Adamki11s <adam@adamki11s.co.uk>
2013-02-04 18:42:27 +0000

    Added Pages class for basic handling of tabulated pages for NPC lists etc

---
+++ src/com/adamki11s/display/Pages.java
@@ -0,0 +1,32 @@
+package com.adamki11s.display;
+
+import java.util.LinkedList;
+
+public class Pages {
+
+	final String[] data;
+	final int itemsPerPage;
+
+	public Pages(String[] data, int itemsPerPage) {
+		this.data = data;
+		this.itemsPerPage = Math.abs(itemsPerPage);
+	}
+
+	public String[] getStringsToSend(int page) {
+		int startIndex = this.itemsPerPage * (Math.abs(page) - 1);
+		LinkedList<String> list = new LinkedList<String>();
+		if (page <= this.getPages()) {
+			for (int i = startIndex; i < (startIndex + this.itemsPerPage); i++) {
+				if (this.data.length > i) {
+					list.add(data[i]);
+				}
+			}
+		}
+		return list.toArray(new String[list.size()]);
+	}
+
+	public int getPages() {
+		return (data.length / this.itemsPerPage);
+	}
+
+}
