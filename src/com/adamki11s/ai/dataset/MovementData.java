c89a926f0b088f362487a53eccc3e61492bd008d
Adamki11s <adam@adamki11s.co.uk>
2013-02-13 17:58:22 +0000

    Added Offset cache to prevent doubling checking of blocks

--- src/com/adamki11s/ai/dataset/MovementData.java
+++ src/com/adamki11s/ai/dataset/MovementData.java
@@ -1,5 +1,6 @@
 package com.adamki11s.ai.dataset;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 import org.bukkit.Location;
@@ -16,11 +17,11 @@ public class MovementData {
 
 	public MovementData(Location rootPoint, int minPauseTicks, int maxPauseTicks, int maxVariation) {
 		this.rootPoint = rootPoint;
-		
-		if(this.rootPoint == null){
+
+		if (this.rootPoint == null) {
 			QuestX.logDebug("NULL ROOT POINT PASSED TO MD");
 		}
-		
+
 		this.minPauseTicks = minPauseTicks;
 		this.maxPauseTicks = maxPauseTicks;// max server ticks until the npc
 											// will request a new movement
@@ -36,57 +37,119 @@ public class MovementData {
 		int rx = rootPoint.getBlockX(), ry = rootPoint.getBlockY(), rz = rootPoint.getBlockZ(), dx, dy, dz;
 		World w = this.rootPoint.getWorld();
 		int it = 0;
+
+		ArrayList<Offset> blackList = new ArrayList<Offset>();
+
+		boolean allow = false;
+
 		do {
 			dx = r.nextInt(maxVariation * 2) - maxVariation;
 			dy = r.nextInt(maxVariation * 2) - maxVariation;
-			/*
-			 * add extra checks to stop 'jumping' into caves
-			 * 
-			 * 
-			 * 
-			 * 
-			 * 
-			 */
 			dz = r.nextInt(maxVariation * 2) - maxVariation;
 			it++;
 			if (it > this.failsafeIterations) {
 				QuestX.logDebug("ITERATION FAILSAFE TRIGGERED.");
 				break;
 			}
-			if(it % 1000 == 0){
+			if (it % 1000 == 0) {
 				QuestX.logDebug("Delta's = (" + dx + ", " + dy + ", " + dz);
 			}
-		} while (!canMoveHere(w, dx, dy, dz, rx, ry, rz));
+
+			Offset o = new Offset((short) dx, (short) dy, (short) dz);
+
+			allow = true;
+
+			for (Offset b : blackList) {
+				if (b.equals(o)) {
+					allow = false;
+				}
+			}
+
+			if (allow && !canMoveHere(w, dx, dy, dz, rx, ry, rz)) {
+				blackList.add(o);
+				allow = false;
+			} else {
+				// allow stays true so break out of while loop
+			}
+
+		} while (!allow);
 
 		if (it > this.failsafeIterations) {
+			QuestX.logDebug("Movement generation failed, MAX iterations");
 			this.endPoint = this.rootPoint;
 			return;
 		} else {
-			this.endPoint = new Location(w, (rx + dx), (ry + dy) - 1, (rz + dz));
+			QuestX.logDebug("Generated movement after " + it + " iterations");
+			this.endPoint = new Location(w, (rx + dx), (ry + dy), (rz + dz));
 			this.pauseTicks = r.nextInt(maxPauseTicks - minPauseTicks) + minPauseTicks;
 		}
 	}
 
 	private boolean canMoveHere(World w, int dx, int dy, int dz, int rx, int ry, int rz) {
 		if (dx == 0 && dy == 0 && dz == 0) {
+			QuestX.logDebug("dx, dy and dx = 0!");
 			return false;
 		}
-		Block b = w.getBlockAt((rx + dx), (ry + dy) - 1, (rz + dz));//block it stands on
-		return (!b.isLiquid() && b.getTypeId() != 0 && b.getRelative(0, 1, 0).getTypeId() == 0 && b.getRelative(0, 2, 0).getTypeId() == 0
-				&& b.getRelative(1, 0, 0).getTypeId() == 0 && b.getRelative(-1, 0, 0).getTypeId() == 0&& b.getRelative(0, 0, 1).getTypeId() == 0
-				&& b.getRelative(0, 0, -1).getTypeId() == 0);
-	}
-	
-	private boolean isHighestNonAirBlock(Block b){
-		for(int i = 128; i > 1; i--){
-			Location l = new Location(b.getWorld(),b.getX(), i, b.getZ());
-			if(b.getWorld().getBlockAt(l).getTypeId() == 0){
-				continue;
-			} else {
-				return (b.getY() == l.getY());
-			}
-		}
-		return true;
+
+		Block b = w.getBlockAt((rx + dx), (ry + dy), (rz + dz));// block it
+		// stands on
+		/*
+		 * return (!b.isLiquid() && b.getTypeId() != 0 && b.getRelative(0, 1,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 2, 0).getTypeId() == 0 &&
+		 * b.getRelative(1, 0, 0).getTypeId() == 0 && b.getRelative(-1, 0,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 0, 1).getTypeId() == 0 &&
+		 * b.getRelative(0, 0, -1).getTypeId() == 0);
+		 */
+
+		/*
+		 * if((!b.isLiquid() && b.getTypeId() != 0 && b.getRelative(0, 1,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 2, 0).getTypeId() == 0 &&
+		 * b.getRelative(1, 0, 0).getTypeId() == 0 && b.getRelative(-1, 0,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 0, 1).getTypeId() == 0 &&
+		 * b.getRelative(0, 0, -1).getTypeId() == 0)){
+		 */
+
+		/*
+		 * if (!b.isLiquid() && this.isSolid(b.getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 1, 0).getTypeId()) &&
+		 * b.getRelative(0, 2, 0).getTypeId() == 0 &&
+		 * canBlockBeWalkedThrough(b.getRelative(1, 0, 0).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(-1, 0, 0).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 0, 1).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 0, -1).getTypeId())) { //
+		 * b.setType(Material.EMERALD_BLOCK); return true; } else {
+		 * 
+		 * return false; }
+		 */
+
+		/*
+		 * if((b.getTypeId() != 0) && (b.getRelative(0, 1, 0).getTypeId()) == 0
+		 * && (b.getRelative(0, 2, 0).getTypeId() == 0)){ return true; } else {
+		 * return false; }
+		 */
+
+		return this.isSolid(b.getTypeId()) && this.canBlockBeWalkedThrough(b.getRelative(0, 1, 0).getTypeId()) && b.getRelative(0, 2, 0).getTypeId() == 0;
+
+		/*
+		 * if (!b.isLiquid() && b.getTypeId() != 0 && b.getRelative(0, 1,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 2, 0).getTypeId() == 0 &&
+		 * b.getRelative(1, 1, 0).getTypeId() == 0 && b.getRelative(-1, 1,
+		 * 0).getTypeId() == 0 && b.getRelative(0, 1, 1).getTypeId() == 0 &&
+		 * b.getRelative(0, 1, -1).getTypeId() == 0) {
+		 * b.setType(Material.EMERALD_BLOCK); return true; } else {
+		 * 
+		 * return false; }
+		 */
+
+		/*
+		 * return if(!b.isLiquid() && this.isSolid(b.getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 1, 0).getTypeId()) &&
+		 * b.getRelative(0, 2, 0).getTypeId() == 0 &&
+		 * canBlockBeWalkedThrough(b.getRelative(1, 0, 0).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(-1, 0, 0).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 0, 1).getTypeId()) &&
+		 * canBlockBeWalkedThrough(b.getRelative(0, 0, -1).getTypeId()));
+		 */
 	}
 
 	public Location getEndPoint() {
@@ -95,6 +158,15 @@ public class MovementData {
 
 	public int getPauseTicks() {
 		return this.pauseTicks;
+	}
+
+	private boolean isSolid(int i) {
+		return (i != 10 && i != 11 && i != 51 && i != 59 && i != 65 && i != 0);
+	}
+
+	private boolean canBlockBeWalkedThrough(int id) {
+		return (id == 0 || id == 6 || id == 50 || id == 63 || id == 30 || id == 31 || id == 32 || id == 37 || id == 38 || id == 39 || id == 40 || id == 55 || id == 66 || id == 75
+				|| id == 76 || id == 78);
 	}
 
 }
