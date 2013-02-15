95d9a0c8eca0e9948da04ba3d9cc1c0a512b2630
Adamki11s <adam@adamki11s.co.uk>
2013-02-15 17:08:49 +0000

    Play record now exclusively effects the player

--- src/com/adamki11s/npcs/triggers/action/PlayRecordAction.java
+++ src/com/adamki11s/npcs/triggers/action/PlayRecordAction.java
@@ -1,7 +1,6 @@
 package com.adamki11s.npcs.triggers.action;
 
 import org.bukkit.Effect;
-import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import com.adamki11s.questx.QuestX;
@@ -10,28 +9,19 @@ public class PlayRecordAction implements Action {
 
 	private boolean isActive = true;
 
-	private int recordID, range;
-
-	private long lastPlay = -1, playLengthSeconds = -1;
+	private int recordID;
 
 	// PLAY_RECORD:<record block id>,<range>
 	public PlayRecordAction(String npc, String data) {
-		String[] split = data.split("#");
 		try {
-			recordID = Integer.parseInt(split[0]);
-			range = Integer.parseInt(split[1]);
-			if(range <= 0){
-				this.isActive = false;
-				QuestX.logError("Block record play range must be at least 1 '" + npc + ". Setting disabled.");
-				return;
-			}
+			recordID = Integer.parseInt(data);
 		} catch (NumberFormatException e) {
 			this.isActive = false;
 			QuestX.logError("Error parsing record id/range in custom_trigger file for NPC '" + npc + ". Setting disabled.");
 			return;
 		}
 		
-		if((this.playLengthSeconds = this.getTrackLengthSeconds()) == -1){
+		if(!this.isTrackValid()){
 			this.isActive = false;
 			QuestX.logError("Invalid record block id specified for NPC '" + npc + ". Setting disabled.");
 			return;
@@ -40,17 +30,7 @@ public class PlayRecordAction implements Action {
 
 	@Override
 	public void implement(Player p) {
-		if(lastPlay != -1){
-			long milliDiff = System.currentTimeMillis() - this.lastPlay;
-			long secondsDiff = (milliDiff / 1000);
-			if(secondsDiff < this.playLengthSeconds){
-				//if track has not finished playing cancel
-				return;
-			}
-		}
-		World w = p.getWorld();
-		w.playEffect(p.getLocation(), Effect.RECORD_PLAY, recordID, range);
-		this.lastPlay = System.currentTimeMillis();
+		p.playEffect(p.getLocation(), Effect.RECORD_PLAY, recordID);
 	}
 
 	@Override
@@ -73,34 +53,13 @@ public class PlayRecordAction implements Action {
 	 * 2267 Music Disk (Wait) 3:58
 	 */
 
-	private long getTrackLengthSeconds() {
-		switch (recordID) {
-		case 2256:
-			return 178;
-		case 2257:
-			return 185;
-		case 2258:
-			return 345;
-		case 2259:
-			return 185;
-		case 2260:
-			return 174;
-		case 2261:
-			return 197;
-		case 2262:
-			return 96;
-		case 2263:
-			return 150;
-		case 2264:
-			return 188;
-		case 2265:
-			return 251;
-		case 2266:
-			return 71;
-		case 2267:
-			return 238;
+	private boolean isTrackValid() {
+		for(int rec = 2256; rec <= 2267; rec++){
+			if(rec == this.recordID){
+				return true;
+			}
 		}
-		return -1;
+		return false;
 	}
 
 }
