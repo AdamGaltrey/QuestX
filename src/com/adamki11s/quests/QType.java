ebad50e83fc430800a3fa5da486dd4cbcef9c7c4
Adamki11s <adam@adamki11s.co.uk>
2013-02-16 00:12:42 +0000

    Added GOTO location quest type

--- src/com/adamki11s/quests/QType.java
+++ src/com/adamki11s/quests/QType.java
@@ -2,25 +2,22 @@ package com.adamki11s.quests;
 
 public enum QType {
 
-	FETCH_ITEMS,
-	KILL_ENTITIES,
-	KILL_NPC,
-	TALK_NPC;
-	
-	public static QType parseType(String type){
-		if(type.equalsIgnoreCase("FETCH_ITEMS")){
+	FETCH_ITEMS, GOTO, KILL_ENTITIES, KILL_NPC, TALK_NPC;
+
+	public static QType parseType(String type) {
+		if (type.equalsIgnoreCase("FETCH_ITEMS")) {
 			return FETCH_ITEMS;
-		} else if(type.equalsIgnoreCase("KILL_ENTITIES")){
+		} else if (type.equalsIgnoreCase("KILL_ENTITIES")) {
 			return KILL_ENTITIES;
-		} else if(type.equalsIgnoreCase("KILL_NPC")){
+		} else if (type.equalsIgnoreCase("KILL_NPC")) {
 			return KILL_NPC;
-		}  else if(type.equalsIgnoreCase("TALK_NPC")){
+		} else if (type.equalsIgnoreCase("TALK_NPC")) {
 			return TALK_NPC;
+		} else if (type.equalsIgnoreCase("GOTO")) {
+			return GOTO;
 		} else {
 			return null;
 		}
 	}
-	
-	
-	
+
 }
