9a2a26d983df8b1e3c9cc23e8173459581ff00c3
Adamki11s <adam@adamki11s.co.uk>
2013-02-17 00:00:35 +0000

    Begun class for help book

---
+++ src/com/adamki11s/commands/help/HelpBook.java
@@ -0,0 +1,16 @@
+package com.adamki11s.commands.help;
+
+public class HelpBook {
+	
+	/*ItemStack i = new ItemStack(Material.WRITTEN_BOOK, 1);
+	BookMeta bm = (BookMeta)i.getItemMeta();
+	bm.setAuthor("Fireblast709");
+	bm.setTitle("Cooking pork in 50 ways");
+	ArrayList<String> pages = new ArrayList<String>();
+	pages.add("With vire");
+	pages.add("In a furnace");
+	bm.setPages(pages);
+	bm.setPage(0, "With fire");
+	i.setItemMeta(bm);*/
+
+}
