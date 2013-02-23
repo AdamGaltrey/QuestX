e3ace5ddc0a52d70c4f23b191422d6181394c40e
Adamki11s <adam.galtrey@gmail.com>
2013-02-23 15:32:46 +0000

    GlobalDensity won't update if the table doesn't exist

--- src/com/adamki11s/npcs/population/GlobalDensityCache.java
+++ src/com/adamki11s/npcs/population/GlobalDensityCache.java
@@ -14,40 +14,43 @@ public class GlobalDensityCache {
 
 	public static void updateGlobalDensity(SyncSQL sql, String[] worlds) {
 		for (String w : worlds) {
-			if(!nodes.containsKey(w)){
+			if (!nodes.containsKey(w)) {
 				nodes.put(w, new LinkedHashMap<NPCChunkData, Long>());
 			}
-			String query = "SELECT * FROM '" + w + "' ORDER BY density ASC";
-			long gDensity = 0;
 			try {
-				ResultSet set = sql.sqlQuery(query);
-				
-				
-				/*
-				 * Error on GET result set
-				 * 
-				 * No table found 'name', missiing database throwing exceptions?
-				 * 
-				 * java.sql.SQLException: [SQLITE_ERROR] SQL error or missing database (near "-": syntax error)
-				 * 
-				 */
-				
-				
-				while (set.next()) {
-					NPCChunkData cd = new NPCChunkData(set.getInt("x"), set.getInt("z"));
-					gDensity += set.getLong("density");
-					nodes.get(w).put(cd, gDensity);
+				if (sql.doesTableExist(w)) {
+					String query = "SELECT * FROM '" + w + "' ORDER BY density ASC";
+					long gDensity = 0;
+
+					ResultSet set = sql.sqlQuery(query);
+
+					/*
+					 * Error on GET result set
+					 * 
+					 * No table found 'name', missiing database throwing
+					 * exceptions?
+					 * 
+					 * java.sql.SQLException: [SQLITE_ERROR] SQL error or
+					 * missing database (near "-": syntax error)
+					 */
+
+					while (set.next()) {
+						NPCChunkData cd = new NPCChunkData(set.getInt("x"), set.getInt("z"));
+						gDensity += set.getLong("density");
+						nodes.get(w).put(cd, gDensity);
+					}
+					set.close();
+
+					if (gDensities.containsKey(w)) {
+						if (gDensity > gDensities.get(w)) {
+							gDensities.put(w, gDensity);
+						}
+					} else {
+						gDensities.put(w, gDensity);
+					}
 				}
-				set.close();
 			} catch (SQLException e) {
 				e.printStackTrace();
-			}
-			if (gDensities.containsKey(w)) {
-				if (gDensity > gDensities.get(w)) {
-					gDensities.put(w, gDensity);
-				}
-			} else {
-				gDensities.put(w, gDensity);
 			}
 		}
 	}
@@ -55,8 +58,8 @@ public class GlobalDensityCache {
 	public static long getGlobalDensity(String world) {
 		return gDensities.get(world);
 	}
-	
-	public static LinkedHashMap<NPCChunkData, Long> getNodes(String world){
+
+	public static LinkedHashMap<NPCChunkData, Long> getNodes(String world) {
 		return nodes.get(world);
 	}
 
