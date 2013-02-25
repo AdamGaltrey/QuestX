9ad43ca7c56c4dbe88961ea765e7f970f228e79b
Adamki11s <adam.galtrey@gmail.com>
2013-02-25 18:18:54 +0000

    Completed file poll, works correctly

--- src/com/adamki11s/poll/FilePoll.java
+++ src/com/adamki11s/poll/FilePoll.java
@@ -25,7 +25,6 @@ public class FilePoll {
 					Date cur = new Date(f.lastModified());
 					long milliSecTime = (present.getTime() - cur.getTime());
 					int hoursDiff = (int) Math.floor((milliSecTime) / (3600000L));
-					System.out.println("Hours diff for NPC's " + ent.getKey() + " task = " + hoursDiff);
 					if (hoursDiff >= ent.getValue()) {
 						// erase file
 						f.delete();
@@ -36,24 +35,18 @@ public class FilePoll {
 
 		for (Entry<String, Integer> ent : PollManager.questPoll.entrySet()) {
 			File questRoot = new File(FileLocator.quest_data_root);
-			System.out.println("Checking quest root = " + ent.getKey());
 			if (!questRoot.exists()) {
-				System.out.println("none existant quest root, returning");
 				continue;
 			} else {
-				System.out.println("Checking main quest folder exists");
 				File root = new File(FileLocator.quest_data_root + File.separator + ent.getKey());
 				if (!root.exists()) {
-					System.out.println("main folder not found, exiting " + ent.getKey());
 					continue;
 				} else {
-					System.out.println("Checking progression folder");
 					File prog = new File(root + File.separator + "Progression");
 					for (File f : prog.listFiles()) {
 						Date cur = new Date(f.lastModified());
 						long milliSecTime = (present.getTime() - cur.getTime());
 						int hoursDiff = (int) Math.floor((milliSecTime) / (3600000L));
-						System.out.println("Hours diff for quest " + ent.getKey() + " = " + hoursDiff);
 						if (hoursDiff >= ent.getValue()) {
 							// erase file
 							f.delete();
